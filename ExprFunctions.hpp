90713380f68400a52cbaa606f58ed61b40d3585b
Yiming Pan <56082650+YimingPan-Code@users.noreply.github.com>
2021-06-29 16:05:28 -0400

    Update ExprFunctions.hpp

--- ExprFunctions.hpp
+++ ExprFunctions.hpp
@@ -90,32 +90,6 @@ namespace UDIMPL {
     return t.weight;
   }
 
-  template<typename tup>
-  inline VERTEX getCc(tup t) {
-    return t.cc;
-  }
-  void init_graph()
-{
-	srand(0);
-	ulong size;
-	begin_index[0] = 0;
-	vector<ulong> g;
-	set<ulong> out;
-	for (ulong i = 0; i < N; ++i)
-	{
-		size = rand() % MAX_OUT_NODE;
-		begin_index[i + 1] = begin_index[i] + size;
-		while (out.size() < size)
-		{
-			out.emplace(rand() % N);
-		}
-		g.insert(g.end(), out.begin(), out.end());
-	}
-	graph = new ulong[g.size()];
-	memcpy(graph, g.data(), g.size() * sizeof(ulong));
-}
-
-
 template <class T>
 double mean(const T X[])
 {
@@ -143,7 +117,7 @@ double variance(const T X[], const double x_mean)
 }
 
 
-//返回样例的标准差
+//Returns the standard deviation of the sample
 template <class T>
 double standardDeviation(const T X[], const double x_mean)
 {
@@ -151,7 +125,7 @@ double standardDeviation(const T X[], const double x_mean)
 }
 
 
-//计算相关系数
+//Calculate the correlation coefficient
 template <class T>
 double sampleCorrelationCoefficient(T X[], T Y[])
 {
@@ -161,6 +135,81 @@ double sampleCorrelationCoefficient(T X[], T Y[])
 	double y_variance = variance(Y, y_mean);
 	double xy_covariance = covariance(X, Y, x_mean, y_mean);
 	return xy_covariance / sqrt(x_variance * y_variance);
+}
+//init graph
+void init_graph()
+{
+	srand(0);
+	ulong size;
+	begin_index[0] = 0;
+	vector<ulong> g;
+	set<ulong> out;
+	for (ulong i = 0; i < N; ++i)
+	{
+		size = rand() % MAX_OUT_NODE;
+		begin_index[i + 1] = begin_index[i] + size;
+		while (out.size() < size)
+		{
+			out.emplace(rand() % N);
+		}
+		g.insert(g.end(), out.begin(), out.end());
+	}
+	graph = new ulong[g.size()];
+	memcpy(graph, g.data(), g.size() * sizeof(ulong));
+}
+int main()
+{
+	ulong current_vertex, out_size;
+	clock_t start, end;
+	init_graph();
+	printf("graph inited\n");
+	printf("number of nodes: %lu\n", N);
+	printf("number of edges: %lu\n", begin_index[N]);
+	printf("average number of edges: %.2lf\n", double(begin_index[N]) / N);
+	start = clock();
+	ulong total_iteration_time = 0;
+	for (ulong source = 0; source < N; ++source)
+	{
+		if (begin_index[source] == begin_index[source + 1])
+			continue;
+		memset(count, 0, N * sizeof(ulong));
+		memset(old_count, 0, N * sizeof(ulong));
+		current_vertex = source;
+		int iter;
+		for (iter = 0; iter < MAX_ITER; ++iter)
+		{
+			out_size = begin_index[current_vertex + 1] - begin_index[current_vertex];
+			if (out_size == 0 || rand() < alpha * RAND_MAX)
+				current_vertex = source; //
+			else
+			{
+				current_vertex = graph[begin_index[current_vertex] + rand() % out_size];
+			}
+			++count[current_vertex];
+			if ((iter + 1) % STEP == 0)
+			{
+				if (iter + 1 > STEP)
+				{
+					double r = sampleCorrelationCoefficient(old_count, count);
+					if (r > 0.999)
+					{
+						++iter;
+						break;
+					}
+				}
+				memcpy(old_count, count, N * sizeof(ulong));
+			}
+		}
+		total_iteration_time += iter;
+		for (ulong i = 0; i < N; ++i)
+			PR[i] += double(count[i]) / (iter * N);
+	}
+	delete[] graph;
+	end = clock();
+	printf("total iteration time: %lu\n", total_iteration_time);
+	printf("average iteration time: %.2lf\n", double(total_iteration_time) / N);
+	printf("total execution time: %.2lf ms\n", (1000.0 * (end - start)) / CLOCKS_PER_SEC);
+	return 0;
 }
   
 }
